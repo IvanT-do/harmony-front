dad60f55b8088e4c1ff9269f0e31fa273e0a8898
Иван Тогузов <ivan.321@list.ri>
2024-05-14 19:55:18 +0300

    Добавлен лоадер

---
+++ src/components/LoaderPage/index.jsx
@@ -0,0 +1,11 @@
+import Logo from "../Icons/Logo";
+
+import "./style.scss";
+
+export default function LoaderPage() {
+    return (
+        <div className="loader-page">
+            <Logo className="loader-page__logo" />
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React, {Suspense} from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.jsx";
+import LoaderPage from "./components/LoaderPage";
 
 import './fonts.css';
 import './index.scss';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Suspense fallback={<div></div>}>
+      <Suspense fallback={<LoaderPage />}>
           <RouterProvider router={router} />
       </Suspense>
   </React.StrictMode>,
