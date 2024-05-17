5dcd56bc393fbd5541baf9eaf4dde6a469115a78
Иван Тогузов <ivan.321@list.ri>
2024-05-17 17:39:06 +0300

    Заполнена страница контактов

---
+++ src/components/Icons/Mail/index.jsx
@@ -0,0 +1,10 @@
+import SvgIcon from "../SvgIcon";
+
+export default function Mail({...props}) {
+    return (
+        <SvgIcon viewBox="0 0 17 17" {...props} fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M12.041 14.5208H4.95768C2.83268 14.5208 1.41602 13.4583 1.41602 10.9791V6.02079C1.41602 3.54163 2.83268 2.47913 4.95768 2.47913H12.041C14.166 2.47913 15.5827 3.54163 15.5827 6.02079V10.9791C15.5827 13.4583 14.166 14.5208 12.041 14.5208Z"/>
+            <path d="M12.0423 6.375L9.82523 8.14583C9.09565 8.72667 7.89856 8.72667 7.16898 8.14583L4.95898 6.375"/>
+        </SvgIcon>
+    );
+}
--- src/routes/Contacts/index.jsx
+++ src/routes/Contacts/index.jsx
@@ -1,5 +1,57 @@
+import VK from "../../components/Icons/VK";
+import Mail from "../../components/Icons/Mail";
+import Phone from "../../components/Icons/Phone";
+
+import "./style.scss";
+
+const ContactsLink = ({ icon, color, children }) => {
+    return (
+        <div className="contacts-link">
+            <div
+                className="contacts-link__icon"
+                style={{
+                    background: color
+                }}
+            >
+                { icon }
+            </div>
+            <div className="contacts-link__text">
+                { children }
+            </div>
+        </div>
+    );
+}
+
 export default function ContactsPage() {
     return (
-        <div>Контакты</div>
+        <section className="contacts">
+            <div className="contacts__content">
+                <h2 className="contacts__title">
+                    Как добраться?
+                </h2>
+                <p className="contacts__address">
+                    Мы находимся по адресу г. Москва, Саринский проезд, д.49
+                </p>
+
+                <div className="contacts__links">
+                    <ContactsLink color="#667AFB" icon={<Phone />}>
+                        Телефон: <a href="tel:+79656532426">+7 965 653 24 26</a>
+                    </ContactsLink>
+
+                    <ContactsLink color="#F9367C" icon={<Mail />}>
+                        Почта: <a href="mailto:garmony@mail.ru">garmony@mail.ru</a>
+                    </ContactsLink>
+
+                    <ContactsLink color="#4891FF" icon={<VK style={{ fontSize: 8 }} />}>
+                        ВКонтакте: <a href="https://vk.com/garmony17284">https://vk.com/garmony17284</a>
+                    </ContactsLink>
+                </div>
+            </div>
+            <iframe
+                src="https://yandex.ru/map-widget/v1/?ll=37.655147%2C55.729566&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCggxMDA1MDEwORI70KDQvtGB0YHQuNGPLCDQnNC-0YHQutCy0LAsINCh0LDRgNC40L3RgdC60LjQuSDQv9GA0L7QtdC30LQiCg0WoBZCFWbrXkI%2C&z=17.47"
+                allowFullScreen={true}
+                className="contacts__map"
+            />
+        </section>
     );
 }
